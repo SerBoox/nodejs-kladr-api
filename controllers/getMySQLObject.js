001ca8fb29bb0ee658241c2886140fb3ba6b788e
Buharkin Sergey <s.buharkin@entirec.ru>
2015-12-26 21:57:47 +0300

    Механизм импорта готов, переключение таблиц производиться в ручную

---
+++ controllers/getMySQLObject.js
@@ -0,0 +1,70 @@
+function getMySQLObject(mysql_table, data) {
+    switch (mysql_table) {
+        case('altnames'):
+            return object = {
+                id: data[0].id,
+                oldcode: data[1].value,
+                newcode: data[2].value,
+                level: data[3].value
+            };
+            break;
+        case('doma'):
+            return object = {
+                id: data[0].id,
+                name: data[1].value,
+                korp: data[2].value,
+                socr: data[3].value,
+                code: data[4].value,
+                index: data[5].value,
+                gninmb: data[6].value,
+                uno: data[7].value,
+                ocatd: data[8].value
+            };
+            break;
+        case('flat'):
+            return object = {
+                id: data[0].id,
+                code: data[1].value,
+                np: data[2].value,
+                gninmb: data[3].value,
+                name: data[4].value,
+                index: data[5].value,
+                uno: data[6].value
+            };
+            break;
+        case("kladr"):
+            return object = {
+                id: data[0].id,
+                name: data[1].value,
+                socr: data[2].value,
+                code: data[3].value,
+                index: data[4].value,
+                gninmb: data[5].value,
+                uno: data[6].value,
+                ocatd: data[7].value,
+                status: data[8].value
+            };
+            break;
+        case('socrbase'):
+            return object = {
+                id: data[0].id,
+                level: data[1].value,
+                scname: data[2].value,
+                socrname: data[3].value,
+                kod_t_st: data[4].value
+            };
+            break;
+        case('street'):
+            return object = {
+                id: data[0].id,
+                altnames: data[1].value,
+                doma: data[2].value,
+                flat: data[3].value,
+                kladr: data[4].value,
+                socrbase: data[5].value,
+                street: data[6].value
+            };
+            break;
+    }
+}
+module.exports = getMySQLObject
--- routes/api.js
+++ routes/api.js
@@ -4,59 +4,134 @@ DBFParser = require('dbfkit-fork/lib/DBFParser');
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
 var merge = require('merge'), original, cloned;
+var mysql = require('mysql');
+var parameters = require('../config/parameters.json');
+var getMySQLObject = require('../controllers/getMySQLObject.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-    var data = [];
+    var request = [];
     var i = 0;
+
+    var record;
+    var connection;
     var recordsCount = 0;
 
-    var pathName = './public/dbf/';
+    //Parameters MySQL connection
+    var tableMySQL = parameters.DataBase.kladr_dbf;
+    connection = mysql.createConnection({
+        host: tableMySQL.host,
+        port: tableMySQL.port,
+        database: tableMySQL.name,
+        user: tableMySQL.user,
+        password: tableMySQL.password
+    });
+    //MySQL Connection
+    connection.connect(function () {
+        console.log('Start MySQL connection');
+    });
 
-    //var fileName = 'ALTNAMES.DBF'; //62 655 строк
-    //var fileName = 'DOMA.DBF'; //2 657 477 строк
-    //var fileName = 'FLAT.DBF'; //0 строк
-    //var fileName = 'KLADR.DBF'; //220 186 строк
-    var fileName = 'SOCRBASE.DBF'; //167 строк
-    //var fileName = 'STREET.DBF'; //1 103 363 строк
+    //var tableDBF = parameters.DBF.ALTNAMES;
+    //var tableDBF = parameters.DBF.DOMA;
+    //var tableDBF = parameters.DBF.FLAT;
+    //var tableDBF = parameters.DBF.KLADR;
+    var tableDBF = parameters.DBF.SOCRBASE;
+    //var tableDBF = parameters.DBF.STREET;
 
-    dbfParser = new DBFParser(pathName + fileName, "CP866");
+    dbfParser = new DBFParser(tableDBF.path + tableDBF.file, tableDBF.charset);
 
     dbfParser.on('head', function (head) {
         //return console.log(head);
         recordsCount = head.recordsCount;
+        //Указываем максимальное число эмиттеров
+        eventEmitter.setMaxListeners(recordsCount);
     });
 
-    dbfParser.on('record', function (record) {
-        //console.log(record);
-        //eventEmitter.emit('dataModified', record);
+    dbfParser.on('record', function (data) {
+        i++;
+        if (Array.isArray(data)) {
+            if (data[0].id === undefined)
+                data.unshift({id: i});
+            else
+                data[0] = {id: i};
+
+            data.forEach(function(currentValue,index){
+              if(currentValue.value == null)
+                  data[index].value = '';
+            });
+
+            var object = getMySQLObject(tableDBF.mysql_table,data);
+
+            //Enable Record Emitter
+            eventEmitter.emit('record_mysql_table', connection, tableDBF.mysql_table, object, recordsCount);
+
+            //Buffering Requesr Result
+            //request.push(data);
+        }
+
+        //Show result
+        if (i === recordsCount) {
+            res.send(data);
+        }
     });
+    i = 0;
 
     dbfParser.on('end', function () {
-        //return console.log('finish');
+        return console.log('Finish read file: ' + tableDBF.file);
     });
 
     dbfParser.parse();
 
-    eventEmitter.on('dataModified', function (record) {
-        i++;
-        if (Array.isArray(record)){
-            if(record[0].id === undefined)
-                record.unshift({id:i});
-            else
-                record[0] = {id:i};
-            //Buffering
-            data.push(record);
+});
+
+eventEmitter.on('reset_mysql_table', function (connection, mysql_table) {
+    //Clear Table "TRUNCATE TABLE  `socrbase`"
+    connection.query('TRUNCATE TABLE  ??', mysql_table,
+        function (error) {
+            if (error !== null) {
+                console.log("MySQL Clear Table Error: " + error);
+            }
         }
-        //Show result
-        if (i === recordsCount) {
-            //data = JSON.stringify(data);
-            res.send(data);
-            //res.send(record);
+    );
+});
+
+eventEmitter.on('reset_mysql_table', function (connection, mysql_table) {
+    //Reset AUTO_INCREMENT values  "ALTER TABLE  `socrbase` AUTO_INCREMENT = 1;"
+    connection.query('ALTER TABLE  ?? AUTO_INCREMENT = 1', mysql_table,
+        function (error) {
+            if (error !== null) {
+                console.log("MySQL Reset AUTO_INCREMENT Error: " + error);
+            }
         }
-    });
-    i = 0;
+    );
+});
+
+var j = 0;
+var startDbRecordTime; //milliseconds
+var finishDbRecordTime; //milliseconds
+eventEmitter.on('record_mysql_table', function (connection, mysql_table, data, recordsCount) {
+    j++;
+    if (j === 1) {
+        eventEmitter.emit('reset_mysql_table', connection, mysql_table);
+        startDbRecordTime = new Date().getTime();
+    }
+
+    connection.query('INSERT INTO ?? SET ?', [mysql_table, data],
+        function (error) {
+            if (error !== null) {
+                console.log("MySQL id: " + j);
+                console.log("MySQL Error: " + error);
+            }
+        }
+    );
+    if (j === recordsCount) {
+        connection.end(function () {
+            finishDbRecordTime = new Date().getTime();
+            console.log('Finish MySQL connection.Record time: ' + (finishDbRecordTime - startDbRecordTime));
+        });
+        j = 0;
+    }
 });
 
 module.exports = router;
