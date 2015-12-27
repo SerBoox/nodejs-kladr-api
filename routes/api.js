de1fefc5ba558103503805b97d528e58f34e1603
Buharkin Sergey <s.buharkin@entirec.ru>
2015-12-27 21:13:58 +0300

    Огромное число правок связанных с доработкой *.DBF парсера

--- routes/api.js
+++ routes/api.js
@@ -1,44 +1,30 @@
 var express = require('express');
 var router = express.Router();
-DBFParser = require('dbfkit-fork/lib/DBFParser');
+//DBFParser = require('dbfkit-fork/lib/DBFParser');
+ParserDBF = require('../controllers/ParserDBF');
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
 var merge = require('merge'), original, cloned;
 var mysql = require('mysql');
 var parameters = require('../config/parameters.json');
 var getMySQLObject = require('../controllers/getMySQLObject.js');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var record, connection;
+    var record, recordsCount;
     var i = 0;
     var request = [];
-    var rowsNumder = 1;
-    var recordsCount = 0;
+    var rowsNumder = 622;
 
-    //Parameters MySQL connection
-    var tableMySQL = parameters.DataBase.kladr_dbf;
-    connection = mysql.createConnection({
-        host: tableMySQL.host,
-        port: tableMySQL.port,
-        database: tableMySQL.name,
-        user: tableMySQL.user,
-        password: tableMySQL.password
-    });
-    //MySQL Connection
-    connection.connect(function () {
-        console.log('Start MySQL connection');
-    });
-
-    var tableDBF = parameters.DBF.ALTNAMES;
-    //var tableDBF = parameters.DBF.DOMA;
+    //var tableDBF = parameters.DBF.ALTNAMES;
+    var tableDBF = parameters.DBF.DOMA;
     //var tableDBF = parameters.DBF.FLAT;
     //var tableDBF = parameters.DBF.KLADR;
     //var tableDBF = parameters.DBF.SOCRBASE;
     //var tableDBF = parameters.DBF.STREET;
 
-
-    dbfParser = new DBFParser(tableDBF.path + tableDBF.file, tableDBF.charset);
+    dbfParser = new ParserDBF(tableDBF.path + tableDBF.file, tableDBF.charset, rowsNumder);
 
     dbfParser.on('head', function (head) {
         //return console.log(head);
@@ -46,6 +32,9 @@ router.get('/', function (req, res, next) {
             recordsCount = head.recordsCount;
         else
             recordsCount = rowsNumder;
+
+        console.log('Start Read File: ' + tableDBF.file + '.Row: ' + recordsCount);
+
         //Указываем максимальное число эмиттеров
         if (recordsCount < 70)
             eventEmitter.setMaxListeners(70);
@@ -63,6 +52,8 @@ router.get('/', function (req, res, next) {
             else
                 data[0] = {id: i};
 
+            console.log("record id: " + i);
+
             data.forEach(function (currentValue, index) {
                 if (currentValue.value == null)
                     data[index].value = '';
@@ -71,26 +62,76 @@ router.get('/', function (req, res, next) {
             var object = getMySQLObject(tableDBF.mysql_table, data);
 
             //Enable Record Emitter
-            eventEmitter.emit('record_mysql_table', connection, tableDBF.mysql_table, object, recordsCount);
-
-            //Buffering Requesr Result
-            //request.push(data);
-        }
+            eventEmitter.emit('record_mysql_table', tableDBF.mysql_table, object, recordsCount);
+        } else
+            return;
 
         //Show result
         if (i === recordsCount) {
-            res.send(data);
+            res.send(data); //Show data
         }
     });
     i = 0;
 
     dbfParser.on('end', function () {
+        if (recordsCount == 0) res.send('File: ' + tableDBF.file + ' is empty.'); //Show Data
         return console.log('Finish read file: ' + tableDBF.file);
     });
 
     dbfParser.parse();
-
 });
+
+var j = 0;
+var startDbRecordTime; //milliseconds
+var finishDbRecordTime; //milliseconds
+var connection;
+
+eventEmitter.on('record_mysql_table', function (mysql_table, data, recordsCount) {
+    j++;
+    if (j == 1)
+        connection = startMySQLConnection();
+
+    if (j == 1) {
+        eventEmitter.emit('reset_mysql_table', connection, mysql_table);
+        startDbRecordTime = new Date().getTime();
+    }
+
+    connection
+        .query('INSERT INTO ?? SET ?', [mysql_table, data],
+            function (error) {
+                if (error !== null) {
+                    console.log("MySQL id: " + j);
+                    console.log("MySQL Error: " + error);
+                }
+            }
+        );
+
+    if (j == recordsCount) {
+        connection.end(function () {
+            finishDbRecordTime = new Date().getTime();
+            console.log('Finish MySQL connection.Record time: ' + (finishDbRecordTime - startDbRecordTime));
+        });
+        j = 0;
+    }
+});
+
+function startMySQLConnection() {
+    //Parameters MySQL connection
+    var tableMySQL = parameters.DataBase.kladr_dbf;
+    var connection = mysql.createConnection({
+        host: tableMySQL.host,
+        port: tableMySQL.port,
+        connectTimeout: 120000,
+        database: tableMySQL.name,
+        user: tableMySQL.user,
+        password: tableMySQL.password
+    });
+    //MySQL Connection
+    connection.connect(function () {
+        console.log('Start MySQL connection');
+    });
+    return connection;
+}
 
 eventEmitter.on('reset_mysql_table', function (connection, mysql_table) {
     //Clear Table "TRUNCATE TABLE  `socrbase`"
@@ -98,7 +139,8 @@ eventEmitter.on('reset_mysql_table', function (connection, mysql_table) {
         function (error) {
             if (error !== null) {
                 console.log("MySQL Clear Table Error: " + error);
-            }
+            } else
+                console.log('Очистка базы: `' + mysql_table + '` прошла успено!');
         }
     );
 });
@@ -112,36 +154,6 @@ eventEmitter.on('reset_mysql_table', function (connection, mysql_table) {
             }
         }
     );
-});
-
-var j = 0;
-var startDbRecordTime; //milliseconds
-var finishDbRecordTime; //milliseconds
-eventEmitter.on('record_mysql_table', function (connection, mysql_table, data, recordsCount) {
-    j++;
-
-    if (j == 1) {
-        console.log('Очистка базы: `'+ mysql_table +'` запущенна');
-        eventEmitter.emit('reset_mysql_table', connection, mysql_table);
-        startDbRecordTime = new Date().getTime();
-    }
-
-    connection.query('INSERT INTO ?? SET ?', [mysql_table, data],
-        function (error) {
-            if (error !== null) {
-                console.log("MySQL id: " + j);
-                console.log("MySQL Error: " + error);
-            }
-        }
-    );
-
-    if (j == recordsCount) {
-        connection.end(function () {
-            finishDbRecordTime = new Date().getTime();
-            console.log('Finish MySQL connection.Record time: ' + (finishDbRecordTime - startDbRecordTime));
-        });
-        j = 0;
-    }
 });
 
 module.exports = router;
